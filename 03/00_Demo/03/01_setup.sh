a74ba3e2653ecddee1393a3d88fddda82cb0b43e
dgaravaldi <daniel.garavaldi@gmail.com>
2025-05-21 15:07:25 +0200

    02 sol, 03 demo+intro

---
+++ 03/00_Demo/03/01_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+PARAMS='01_params.conf'
+#??
+#??
+#??
+#??
+#??
+# Build a dockerfile with tag -t
+#??
+# List images
+#??
+
+# Run the Docker container with auto-restart policy
+#??
+
+
